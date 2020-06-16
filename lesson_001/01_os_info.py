323f35120ca8db4726e008c69223907959d2d9f5
user74 <housechel@mail.ru>
2020-06-16 16:27:31 +0500

    Lesson 01-03 - Completed

--- lesson_001/01_os_info.py
+++ lesson_001/01_os_info.py
@@ -2,8 +2,6 @@
 
 # Нужно собрать информацию об операционной системе и версии пайтона
 
-# TODO запустить этот скрипт и закомитить результат его работы (файл os_info.txt)
-
 import platform
 import sys
 
@@ -14,3 +12,4 @@ print(info)
 with open('os_info.txt', 'w', encoding='utf8') as ff:
     ff.write(info)
 
+# зачет!
