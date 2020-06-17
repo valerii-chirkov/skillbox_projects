d0a8448912ae5db6e9a289452fb8b607332758d9
Valerii <valerii_chirkov@bk.ru>
2020-06-17 22:30:17 +0700

    Done

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -3,10 +3,16 @@
 
 # Есть словарь координат городов
 from pprint import pprint
+
 sites = {'Moscow': (550, 370), 'London': (510, 510), 'Paris': (480, 480)}
 
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+
+
+def distance(city1, city2):
+    return ((city1[0] - city2[0]) ** 2 + (city1[1] - city2[1]) ** 2) ** 0.5
+
 
 # Объявляем переменные
 moscow = sites['Moscow']
@@ -14,23 +20,18 @@ london = sites['London']
 paris = sites['Paris']
 
 # Расстояния между городами
-moscow_london = round((((moscow[0] - london[0])**2) + ((moscow[1] - london[1])**2))**0.5)
-moscow_paris = round((((moscow[0] - paris[0])**2) + ((moscow[1] - paris[1])**2))**0.5)
-
-london_moscow = round((((london[0] - moscow[0])**2) + ((london[1] - moscow[1])**2))**0.5)
-london_paris = round((((london[0] - paris[0])**2) + ((london[1] - paris[1])**2))**0.5)
-
-paris_moscow = round((((paris[0] - moscow[0])**2) + ((paris[1] - moscow[1])**2))**0.5)
-paris_london = round((((paris[0] - london[0])**2) + ((paris[1] - london[1])**2))**0.5)
+moscow_london = round(distance(moscow, london))
+moscow_paris = round(distance(moscow, paris))
+london_paris = round(distance(london, paris))
 
 moscow_distance = {'Расстояние от Москвы до Лондона': f'Расстояние от Москвы до Лондона = {moscow_london}',
                    'Расстояние от Москвы до Парижа': f'Расстояние от Москвы до Парижа = {moscow_paris}'}
 
-london_distance = {'Расстояние от Лондона до Москвы': f'Расстояние от Лондона до Москвы = {london_moscow}',
+london_distance = {'Расстояние от Лондона до Москвы': f'Расстояние от Лондона до Москвы = {moscow_london}',
                    'Расстояние от Лондона до Парижа': f'Расстояние от Лондона до Парижа = {london_paris}'}
 
-paris_distance = {'Расстояние от Парижа до Москвы': f'Расстояние от Парижа до Москвы = {paris_moscow}',
-                  'Расстояние от Парижа до Лондона': f'Расстояние от Парижа до Лондона = {paris_london}'}
+paris_distance = {'Расстояние от Парижа до Москвы': f'Расстояние от Парижа до Москвы = {moscow_paris}',
+                  'Расстояние от Парижа до Лондона': f'Расстояние от Парижа до Лондона = {london_paris}'}
 
 
 distances = {moscow_distance['Расстояние от Москвы до Лондона'], moscow_distance['Расстояние от Москвы до Парижа'],
