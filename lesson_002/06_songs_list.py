1880b57091018a8f691e777d313cbb881289a366
Valerii <valerii_chirkov@bk.ru>
2020-06-17 22:55:27 +0700

    Edited

--- lesson_002/06_songs_list.py
+++ lesson_002/06_songs_list.py
@@ -7,40 +7,30 @@
 # более подробно про функцию round смотрите в документации https://docs.python.org/3/search.html?q=round
 
 violator_songs_list = [
-    ['World in My Eyes', 4.86], #0
-    ['Sweetest Perfection', 4.43], #1
-    ['Personal Jesus', 4.56], #2
-    ['Halo', 4.9], #3
-    ['Waiting for the Night', 6.07], #4
-    ['Enjoy the Silence', 4.20], #5
-    ['Policy of Truth', 4.76], #6
-    ['Blue Dress', 4.29], #7
-    ['Clean', 5.83], #8
+    ['World in My Eyes', 4.86],
+    ['Sweetest Perfection', 4.43],
+    ['Personal Jesus', 4.56],
+    ['Halo', 4.9],
+    ['Waiting for the Night', 6.07],
+    ['Enjoy the Silence', 4.20],
+    ['Policy of Truth', 4.76],
+    ['Blue Dress', 4.29],
+    ['Clean', 5.83],
 ]
 
-# Распечатайте общее время звучания трех песен: 'Halo', 'Enjoy the Silence' и 'Clean' в формате
-#   Три песни звучат ХХХ.XX минут
+WORLD_IN_MY_EYES = violator_songs_list[0][1]
+SWEETEST_PERFECTION = violator_songs_list[1][1]
+PERSONAL_JESUS = violator_songs_list[2][1]
+HALO = violator_songs_list[3][1]
+WAITING_FOR_THE_NIGHT = violator_songs_list[4][1]
+ENJOY_THE_SILENCE = violator_songs_list[5][1]
+POLICY_OF_TRUTH = violator_songs_list[6][1]
+BLUE_DRESS = violator_songs_list[7][1]
+CLEAN = violator_songs_list[8][1]
+three_songs_time = round(SWEETEST_PERFECTION + POLICY_OF_TRUTH + BLUE_DRESS)
 
-print(f'Три песни звучат {round(violator_songs_list[3][1] + violator_songs_list[5][1] + violator_songs_list[8][1], 2)} минут')
+print(f'Три песни звучат {round(HALO + ENJOY_THE_SILENCE + CLEAN)} минут')
+print(f'Общее время звучания трех песен: Sweetest Perfection, Policy of Truth и Blue Dress -'
+      f' {round(three_songs_time, 2)} минут')
 
-# Есть словарь песен группы Depeche Mode
-violator_songs_dict = {
-    'World in My Eyes': 4.76,
-    'Sweetest Perfection': 4.43,
-    'Personal Jesus': 4.56,
-    'Halo': 4.30,
-    'Waiting for the Night': 6.07,
-    'Enjoy the Silence': 4.6,
-    'Policy of Truth': 4.88,
-    'Blue Dress': 4.18,
-    'Clean': 5.68,
-}
 
-# Распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
-#   А другие три песни звучат приблизительно ХХХ минут
-
-three_songs_time = violator_songs_dict['Sweetest Perfection'] + violator_songs_dict['Policy of Truth'] + violator_songs_dict['Blue Dress']
-print(f'Общее время звучания трех песен: \'Sweetest Perfection\', \'Policy of Truth\' и \'Blue Dress\' - {round(three_songs_time, 2)} минут')
-
-# Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
-# Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
