11c0cc020d3bea09b8cfefd903a0dc766cab1833
Valerii <valerii_chirkov@bk.ru>
2020-06-18 21:25:24 +0700

    Done

--- lesson_003/02.py
+++ lesson_003/02.py
@@ -20,16 +20,26 @@ paper_x, paper_y = 8, 9
 # paper_x, paper_y = 9, 11
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+# Бумагу можно развернуть, если одной стороной не проходит, поэтому в цикле проверим и другое условие
+fit = ((envelop_x - paper_x) | (envelop_y - paper_y) >= 0)
 
+if fit is True:
+    print('ДА')
+else:
+    fit = (envelop_x - paper_y) | (envelop_y - paper_x) >= 0
+    if fit is True:
+        print('ДА')
+    else:
+        print('НЕТ')
 # Усложненное задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
 # могут быть в диапазоне от 1 до 1000)
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# hole_x, hole_y = 8, 9
-# brick_x, brick_y, brick_z = 11, 10, 2
+# переименовал, тк путаюсь в переменных
+hole_a, hole_b = 8, 9
+brick_x, brick_y, brick_z = 11, 10, 2
 # brick_x, brick_y, brick_z = 11, 2, 10
 # brick_x, brick_y, brick_z = 10, 11, 2
 # brick_x, brick_y, brick_z = 10, 2, 11
@@ -46,7 +56,17 @@ paper_x, paper_y = 8, 9
 # brick_x, brick_y, brick_z = 6, 11, 3
 # brick_x, brick_y, brick_z = 6, 3, 11
 # brick_x, brick_y, brick_z = 3, 6, 11
-# brick_x, brick_y, brick_z = 3, 11, 6
+brick_x, brick_y, brick_z = 3, 11, 6
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+
+# Если две стороны кирпича меньше размера отверстия, но он пройдет, тк оставшаяся сторона будет длинной
+# и не влияет на прохождения кирпича
+def function(num1, num2, num3, num4, num5):
+    if num1 | num2 > (num3 | num4) & (num3 | num5) & (num4 | num5):
+        print('Влезет')
+    else:
+        print('Не влезет')
+
+
+function(hole_a, hole_b, brick_x, brick_y, brick_z)
