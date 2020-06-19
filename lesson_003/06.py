c6d3335d0e1c5dd3ac3200868ace04d6495846ec
Valerii <valerii_chirkov@bk.ru>
2020-06-19 18:21:30 +0700

    Done

--- lesson_003/06.py
+++ lesson_003/06.py
@@ -3,16 +3,43 @@
 # (цикл for)
 
 import simple_draw as sd
-
 rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
                   sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
-# TODO здесь ваш код
+"""
+point = sd.get_point(50, 50)
+color = None
+distance = ((50 - 350) ** 2 + (50 - 450) ** 2) ** 0.5
 
+
+def bubble(point, step):
+    radius = distance
+    for i in range(7):
+        color = rainbow_colors[i]
+        radius += step
+        sd.circle(center_position=point, radius=radius, color=color, width=5)
+
+
+bubble(point=point, step=5)
+"""
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
-# TODO здесь ваш код
+sd.set_screen_size(width=350, height=600)
+point = sd.get_point(460, -200)
+color = sd.COLOR_BLACK
+distance = ((50 - 350) ** 2 + (50 - 450) ** 2) ** 0.5
+
+
+def bubble(point, step):
+    radius = distance
+    for i in range(7):
+        color = rainbow_colors[i]
+        radius += step
+        sd.circle(center_position=point, radius=radius, color=color, width=30)
+
+
+bubble(point=point, step=30)
 
 sd.pause()
