d6ca0fae7f601f5e45e56f266f92c3f850e6439f
Valerii <valerii_chirkov@bk.ru>
2020-06-19 19:05:01 +0700

    Done

--- lesson_002/06_songs_list.py
+++ lesson_002/06_songs_list.py
@@ -18,41 +18,20 @@ violator_songs_list = [
     ['Clean', 5.83],
 ]
 
-# TODO Куда делся словарик ? добавил его снова!
-
-# Есть словарь песен группы Depeche Mode
-violator_songs_dict = {
-    'World in My Eyes': 4.76,
-    'Sweetest Perfection': 4.43,
-    'Personal Jesus': 4.56,
-    'Halo': 4.30,
-    'Waiting for the Night': 6.07,
-    'Enjoy the Silence': 4.6,
-    'Policy of Truth': 4.88,
-    'Blue Dress': 4.18,
-    'Clean': 5.68,
-}
-
 # Распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
 #   А другие три песни звучат приблизительно ХХХ минут
-
-# TODO константы отлично, нужные оставляем не нужные удаляем
-WORLD_IN_MY_EYES = violator_songs_list[0][1]
 SWEETEST_PERFECTION = violator_songs_list[1][1]
-PERSONAL_JESUS = violator_songs_list[2][1]
 HALO = violator_songs_list[3][1]
-WAITING_FOR_THE_NIGHT = violator_songs_list[4][1]
 ENJOY_THE_SILENCE = violator_songs_list[5][1]
 POLICY_OF_TRUTH = violator_songs_list[6][1]
 BLUE_DRESS = violator_songs_list[7][1]
 CLEAN = violator_songs_list[8][1]
-three_songs_time = round(SWEETEST_PERFECTION + POLICY_OF_TRUTH + BLUE_DRESS)
 
-# TODO отлично f строки
-# TODO Вычисления и преобразования из принта выносим в отдельную переменную
-print(f'Три песни звучат {round(HALO + ENJOY_THE_SILENCE + CLEAN)} минут')
-# TODO Этот вывод должен быть по словарю violator_songs_dict
-print(f'Общее время звучания трех песен: Sweetest Perfection, Policy of Truth и Blue Dress -'
-      f' {round(three_songs_time, 2)} минут')
+three_songs_time_SPB = round(SWEETEST_PERFECTION + POLICY_OF_TRUTH + BLUE_DRESS)
+three_songs_titles_SPB = {violator_songs_list[1][0], violator_songs_list[6][0], violator_songs_list[7][0]}
+three_songs_time_HEC = round(HALO + ENJOY_THE_SILENCE + CLEAN)
+
+print(f'Три песни звучат {three_songs_time_HEC} минут')
+print(f'Общее время звучания трех песен: {three_songs_titles_SPB} = {three_songs_time_SPB} минут')
 
 
