33c5d32a0012a97c5b4bbbe025c5802ef8ca5759
Valerii <valerii_chirkov@bk.ru>
2020-06-22 19:25:25 +0700

    Done

--- lesson_003/07.py
+++ lesson_003/07.py
@@ -16,7 +16,7 @@ def bricks1():
     for i in range(20):
         start = simple_draw.get_point(x1, y1)
         end = simple_draw.get_point(x2, y2)
-        lines = simple_draw.line(start_point=start, end_point=end, color=COLOR_BEIGE, width=5)
+        simple_draw.line(start_point=start, end_point=end, color=COLOR_BEIGE, width=5)
         for j in range(10):
             x11 = x1 + (100 * j)
             if i % 2 != 0:
@@ -27,6 +27,11 @@ def bricks1():
             x22, y22 = x11, y1 - 50
             start1 = simple_draw.get_point(x11, y11)
             end1 = simple_draw.get_point(x22, y22)
-            lines1 = simple_draw.line(start_point=start1, end_point=end1, color=COLOR_BEIGE, width=5)
+            simple_draw.line(start_point=start1, end_point=end1, color=COLOR_BEIGE, width=5)
+        y1 -= 50
+        y2 -= 50
+
+
+bricks1()
 
 simple_draw.pause()
