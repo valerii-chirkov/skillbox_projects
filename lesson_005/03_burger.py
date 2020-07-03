1aac26f344f3e5293b309498d8329ebee4ed8974
Valerii <valerii_chirkov@bk.ru>
2020-07-04 00:50:26 +0700

    done

--- lesson_005/03_burger.py
+++ lesson_005/03_burger.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from lesson_005.my_burger import *
 # Создать модуль my_burger. В нем определить функции добавления инградиентов:
 #  - булочки
 #  - котлеты
@@ -11,8 +11,29 @@
 
 # В этом модуле создать рецепт двойного чизбургера (https://goo.gl/zA3goZ)
 # с помощью фукций из my_burger и вывести на консоль.
-
+# add_bun()
+# add_cutlet()
+# add_cheese()
+# add_cutlet()
+# add_cheese()
+# add_cucumber()
+# add_mayonnaise()
+# add_tomato()
+# add_bun()
 # Создать рецепт своего бургера, по вашему вкусу.
 # Если не хватает инградиентов - создать соответствующие функции в модуле my_burger
 
-# TODO здесь ваш код
+
+add_bun()
+add_cutlet()
+add_cheese()
+add_cutlet()
+add_cheese()
+add_onion()
+add_cucumber()
+add_mayonnaise()
+add_french_fries()
+add_tomato()
+add_bun()
+add_sesame()
+
