4a92cbb902496368dcb21e0e422fcfca2dd9ded9
Valerii <valerii_chirkov@bk.ru>
2020-07-04 00:51:00 +0700

    done

---
+++ lesson_005/my_burger.py
@@ -0,0 +1,34 @@
+def add_bun():
+    print('А теперь добавим булочку')
+
+
+def add_cutlet():
+    print('А теперь добавим котлету')
+
+
+def add_cucumber():
+    print('А теперь добавим огурчик')
+
+
+def add_tomato():
+    print('А теперь добавим помидорчик')
+
+
+def add_mayonnaise():
+    print('А теперь добавим майонез')
+
+
+def add_cheese():
+    print('А теперь добавим сыр')
+
+
+def add_onion():
+    print('А теперь добавим сыр')
+
+
+def add_french_fries():
+    print('А теперь добавим картошку фри')
+
+
+def add_sesame():
+    print('А теперь добавим картошку кунжут')
