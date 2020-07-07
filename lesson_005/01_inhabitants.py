7e1a0966364a0f611535a2bfaf0850e2cfd2b1a1
Valerii <valerii_chirkov@bk.ru>
2020-07-07 17:38:21 +0700

    done

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -3,8 +3,9 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-from lesson_005.room_1 import folks
+from lesson_005.room_1 import folks as folks1
+from lesson_005.room_2 import folks as folks2
 
-# TODO Есть еще жильцы в других комнатах!
-# TODO А если жильцов будет больше 100, используйте format, join
-print(f'В комнате room_1 живут: {folks[0]} и {folks[1]}')
+separator = ', '
+print(f'В комнате room_1 живут: {separator.join(folks1)}')
+print(f'В комнате room_2 живут: {separator.join(folks2)}')
