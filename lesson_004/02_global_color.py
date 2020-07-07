07f0c5bd41497e2fb6abd22ac5541052506366fd
user74 <housechel@mail.ru>
2020-07-07 14:03:43 +0500

    Lesson 004 02 - Corrected

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -24,6 +24,7 @@ length = 100
 incline = 20
 
 
+# TODO решить вопрос с delta
 def triangle(start_point, delta, length):
     for delta in range(incline, incline + 251, 120):
         shape_triangle = sd.get_vector(start_point=start_point, angle=delta, length=length, width=2)
