cfa33934da172fce76074f2f7002796a036b23e2
Valerii <valerii_chirkov@bk.ru>
2020-07-07 16:39:53 +0700

    done

--- lesson_004/03_shape_select.py
+++ lesson_004/03_shape_select.py
@@ -12,29 +12,29 @@ import simple_draw as sd
 delta = 0
 length = 100
 
-# TODO решить вопрос с delta
-def triangle(start_point=sd.get_point(250, 250), *, delta, length):
+
+def triangle(start_point=sd.get_point(250, 250), *, length):
     for delta in range(0, 251, 120):
         shape_triangle = sd.get_vector(start_point=start_point, angle=delta, length=length, width=2)
         shape_triangle.draw()
         start_point = shape_triangle.end_point
 
 
-def square(start_point=sd.get_point(250, 250), *, delta, length):
+def square(start_point=sd.get_point(250, 250), *, length):
     for delta in range(0, 351, 90):
         shape_square = sd.get_vector(start_point=start_point, angle=delta, length=length, width=2)
         shape_square.draw()
         start_point = shape_square.end_point
 
 
-def pentagon(start_point=sd.get_point(250, 250), *, delta, length):
+def pentagon(start_point=sd.get_point(250, 250), *, length):
     for delta in range(0, 289, 72):
         shape_pentagon = sd.get_vector(start_point=start_point, angle=delta, length=length, width=2)
         shape_pentagon.draw()
         start_point = shape_pentagon.end_point
 
 
-def hexagon(start_point=sd.get_point(250, 200), *, delta, length):
+def hexagon(start_point=sd.get_point(250, 200), *, length):
     for delta in range(0, 301, 60):
         shape_hexagon = sd.get_vector(start_point=start_point, angle=delta, length=length, width=2)
         shape_hexagon.draw()
@@ -53,7 +53,7 @@ input_shape = input('Введите номер желаемой фигуры: ')
 
 if input_shape in shapes_dict:
     print('Вы ввели ', input_shape)
-    shapes_dict[input_shape][1](delta=delta, length=length)
+    shapes_dict[input_shape][1](length=length)
 else:
     print('Вы ввели неверное число')
     sd.pause()
