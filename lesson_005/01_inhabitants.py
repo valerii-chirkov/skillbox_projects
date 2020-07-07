c1d3500e2880bc26e8bc5e3f0bdd64ce55aa6e9d
user74 <housechel@mail.ru>
2020-07-07 14:19:01 +0500

    Lesson 005 01 - Corrected

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -5,4 +5,6 @@
 
 from lesson_005.room_1 import folks
 
+# TODO Есть еще жильцы в других комнатах!
+# TODO А если жильцов будет больше 100, используйте format, join
 print(f'В комнате room_1 живут: {folks[0]} и {folks[1]}')
