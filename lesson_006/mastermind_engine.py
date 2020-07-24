f7c5af8d0f10d21bc31fb940384b207e254a3125
Valerii <valerii_chirkov@bk.ru>
2020-07-24 10:59:58 +1000

    done

--- lesson_006/mastermind_engine.py
+++ lesson_006/mastermind_engine.py
@@ -9,7 +9,6 @@ from random import shuffle
 # from lesson_006.mastermind import user_number
 from termcolor import colored
 _game_number = ''
-user_number = '1234'
 
 
 def random_number():
@@ -21,18 +20,20 @@ def random_number():
         _game_number += str(get_ten_digits[i])
         if _game_number[0] == '0':
             _game_number = _game_number.replace('0', str(get_ten_digits[5]))
+    print(_game_number)
     return _game_number
 
 
-def guess_number():
+def guess_number(user_number):
     if (len(set(user_number)) == 4) & (len(user_number) == 4):
         return user_number
     else:
         print(colored('Вы ввели неверное число', 'red'))
-    check_number()
+        guess_number(user_number)  # TODO а как можно запустить без рекурсии?
+# TODO Я не совсем понимаю как можно функцию guess_number разделить на две - одну с выводом, а другую с логикой
 
 
-def check_number():
+def check_number(user_number):
     stats = {'bulls': 0, 'cows': 0}
     for i in range(4):
         if user_number[i] == _game_number[i]:
@@ -43,4 +44,4 @@ def check_number():
     return stats
 
 
-# TODO Я не совсем понимаю как можно функцию guess_number разделить на две - одну с выводом, а другую с логикой
+
