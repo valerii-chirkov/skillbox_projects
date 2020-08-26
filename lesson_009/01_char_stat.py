141a8d8d7cf332d02a3ac964c9883e665cad7bc2
Valerii <valerii_chirkov@bk.ru>
2020-08-27 01:06:48 +1000

    done

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import zipfile
 # Подсчитать статистику по буквам в романе Война и Мир.
 # Входные параметры: файл для сканирования
 # Статистику считать только для букв алфавита (см функцию .isalpha() для строк)
@@ -7,7 +7,7 @@
 # Вывести на консоль упорядоченную статистику в виде
 # +---------+----------+
 # |  буква  | частота  |
-# +---------+----------+
+# +---------+----------+  alt + T = †
 # |    А    |   77777  |
 # |    Б    |   55555  |
 # |   ...   |   .....  |
@@ -25,7 +25,69 @@
 #   см https://refactoring.guru/ru/design-patterns/template-method
 #   и https://gitlab.skillbox.ru/vadim_shandrinov/python_base_snippets/snippets/4
 
-# TODO здесь ваш код
+import zipfile
+import operator
+from random import randint
+from pprint import pprint
+
+# zip_file_name = 'voyna-i-mir.txt.zip'
+# zfile = zipfile.ZipFile(zip_file_name, 'r')
+# for filename in zfile.namelist():
+#     zfile.extract(filename)
+class StatMaker:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.stat = {}
+        self.prev_char = ''
+
+    def unzip(self):
+        zfile = zipfile.ZipFile(self.file_name, 'r')
+        for filename in zfile.namelist():
+            zfile.extract(filename)
+        self.file_name = filename
+
+    def open(self):
+        if self.file_name.endswith('.zip'):
+            self.unzip()
+        with open(self.file_name, 'r', encoding='cp1251') as file:
+            for line in file:
+                self.check_alfa(line)
+
+    def check_alfa(self, line):
+        for char in line:
+            if char.isalpha():
+                self.collect_char(char)
+
+    def collect_char(self, char):
+        if self.prev_char in self.stat:
+            if char in self.stat[self.prev_char]:
+                self.stat[self.prev_char][char] += 1
+            else:
+                self.stat[self.prev_char][char] = 1
+        else:
+            self.stat[self.prev_char] = {char: 1}
+
+    def sort_stat_max_to_min(self):
+        sorted_stat = sorted(self.stat[self.prev_char].items(), key=operator.itemgetter(1), reverse=True)
+        return sorted_stat
+
+    def print_stat(self):
+        print(f'+---------+----------+')
+        print(f'|  буква  | частота  |')
+        print(f'+---------+----------+')
+        # pprint(self.sort_stat_max_to_min())
+        for item in self.sort_stat_max_to_min():
+            # print(*item)  # Как заставить работать с f-методом? Выводит все без лишних символов
+            # item[0] = item[0].translate(None, '()'',')  # Подсмотрел такой вариант, но не работает с тьюплами
+            print(f'{item}')
+
+
+statmaker = StatMaker(file_name='voyna-i-mir.txt.zip')
+statmaker.open()
+statmaker.print_stat()
+
+
+
 
 # После зачета первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
