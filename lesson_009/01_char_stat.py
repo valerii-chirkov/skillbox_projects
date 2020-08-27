08ce0d9df41075d48085dbb42760e00e317f6c6d
Valerii <valerii_chirkov@bk.ru>
2020-08-28 01:08:43 +1000

    done

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -27,89 +27,67 @@ import zipfile
 
 import zipfile
 import operator
-from random import randint
 from pprint import pprint
 
-# TODO убирайте такой мертвый код, он только захламляет модуль
-# zip_file_name = 'voyna-i-mir.txt.zip'
-# zfile = zipfile.ZipFile(zip_file_name, 'r')
-# for filename in zfile.namelist():
-#     zfile.extract(filename)
-class StatMaker:  # TODO РЕР8: до и после определения класса должно быть по 2 пустых строки
+
+class StatMaker:
     def __init__(self, file_name):
         self.file_name = file_name
         self.stat = {}
-        self.prev_char = ''
 
-    def unzip(self):
+    def unzip(self):  # 1) получение данных (тут может быть подготовка файла - его разахивация, при необходимости),
         zfile = zipfile.ZipFile(self.file_name, 'r')
         for filename in zfile.namelist():
             zfile.extract(filename)
-        self.file_name = filename
+        self.file_name = filename  # TODO Можно что-то исправить, чтобы не подсвечивало? Или это нормально?
 
-    def open(self):
+    def open(self):  # 1) получение данных (тут может быть подготовка файла - его разахивация, при необходимости),
         if self.file_name.endswith('.zip'):
             self.unzip()
         with open(self.file_name, 'r', encoding='cp1251') as file:
             for line in file:
-                self.check_alfa(line)
+                self.collect_char(line)
 
-    def check_alfa(self, line):
+    def collect_char(self, line):  # 2) сбор даннных - подсчёт частоты использования букв,
         for char in line:
             if char.isalpha():
-                self.collect_char(char)
+                self.stat[char] = self.stat.get(char, 0) + 1
 
-    def collect_char(self, char):
-        if self.prev_char in self.stat:
-            if char in self.stat[self.prev_char]:
-                self.stat[self.prev_char][char] += 1
-            else:
-                self.stat[self.prev_char][char] = 1
-        else:
-            self.stat[self.prev_char] = {char: 1}
-        # todo Постарайтесь упростить до плоского словаря: ключ это символ, а значение его кол-во в тексте. Тогда весь
-        #  код метода можно будет записать в одну строку: self.stat[char] = self.stat.get(char, 0) + 1
+    def sort_chars(self):  # 3) сортировка статистических данных,
+        stat = sorted(self.stat.items(), key=operator.itemgetter(1), reverse=True)
+        return stat
 
-    def sort_stat_max_to_min(self):
-        sorted_stat = sorted(self.stat[self.prev_char].items(), key=operator.itemgetter(1), reverse=True)
-        return sorted_stat
+    def print_stat(self):  # 4) вывод в консоль таблицы с данными.
+        print('+---------+----------+')
+        print('|  буква  | частота  |')
+        print('+---------+----------+')
+        # TODO почему так не работает?, если sort_chars такой:
+        # def sort_chars(self):  # 3) сортировка статистических данных,
+        #     sorted(self.stat.items(), key=operator.itemgetter(1), reverse=True)
+        # TODO а вывод такой:
+        # for char, amount in self.stat:
+        #     pprint(char, amount)
+        # TODO соответственно так тоже не работает:
+        # for item in self.stat:
+        #     print(f'|{item[0]:^5}|{item[1]:^7}|')
+        for item in self.sort_chars():
+            print(f'|{item[0]:^9}|{item[1]:^10}|')
+        print('+---------+----------+')
+        values = self.stat.values()  # TODO хотел перенести их в инит, но не давал вызвать
+        total_chars = sum(values)
+        print(f'|  ИТОГО  |{total_chars:^10}|')  # TODO не разобрался как вставить ^9 в строку ИТОГО
+        print('+---------+----------+')
 
-    def print_stat(self):
-        print(f'+---------+----------+')  # todo А тут и не обязательны f-строки, это же статический текст
-        print(f'|  буква  | частота  |')
-        print(f'+---------+----------+')
-        # pprint(self.sort_stat_max_to_min())
-        for item in self.sort_stat_max_to_min():  # todo C точки зрения реализации паттерна, удобнее тут выводить уже
-            # готовые отсортированные данные, так как все основные шаги-методы должны вызываться в "шаблонном" методе,
-            # то есть в методе печати вызывать сортировку это не правильно.
-
-            # print(*item)  # Как заставить работать с f-методом? Выводит все без лишних символов
-            # item[0] = item[0].translate(None, '()'',')  # Подсмотрел такой вариант, но не работает с тьюплами
-            print(f'|{item[0]:^5}|{item[1]:^7}|')
-            # todo выводите сами данные, а также границы таблицы, для его используйте возможности
-            #  форматирования с указанием ширины места для числа c заполнением остального места пробелами.
-            #  Добавьте также строку с итоговыми данными
+    def launch(self):
+        self.open()  # а) подготовка файла (разархивация, при необходимости)
+        # б) сбор данных из файла  # TODO по шаблонному методу должно быть так (ниже)
+        # self.collect_char(line)  # TODO как вызвать этот метод, если он уже вызывается в .open()?
+        self.sort_chars()  # в) сортировка
+        self.print_stat()  # г) вывод в консоль
 
 
-    # TODO Для реализации паттерна необходимо разбить всю выполняемую задачу на отдельные шаги:
-    #  1) получение данных (тут может быть подготовка файла - его разахивация, при необходимости),
-    #  2) сбор даннных - подсчёт частоты использования букв,
-    #  3) сортировка статистических данных,
-    #  4) вывод в консоль таблицы с данными.
-    #  Также потребуется сам "шаблонный метод", где последовательно вызываются все методы по порядку.
-    #    а) подготовка файла (разархивация, при необходимости)
-    #    б) сбор данных из файла
-    #    в) сортировка
-    #    г) вывод в консоль
-    #  Имя шаблонного метода должно быть кратким, например: "запустить", "выполнить"
-
-statmaker = StatMaker(file_name='voyna-i-mir.txt.zip')
-# TODO Имена файлов надо присваивать константам и использовать в основном коде только их.
-
-statmaker.open()
-statmaker.print_stat()
-# TODO Тут надо вызвать только один метод - шаблонный.
-
+STATMAKER = StatMaker(file_name='voyna-i-mir.txt.zip')
+STATMAKER.launch()
 
 # После зачета первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
