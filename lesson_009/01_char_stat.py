fa4c04d95039e7707ede8b0652e1d0f6e045a92f
Valerii <valerii_chirkov@bk.ru>
2020-08-29 23:22:13 +1000

    done

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -36,32 +36,27 @@ class StatMaker:
         self.file_name = file_name
         self.stat = {}
         self.sorted_stat = []
+        self.file = ''
 
-    def unzip(self):  # 1) получение данных (тут может быть подготовка файла - его разахивация, при необходимости),
-        zfile = zipfile.ZipFile(self.file_name, 'r')
-        self.file_name = ''
-        for filename in zfile.namelist():
-            zfile.extract(filename)
-        self.file_name = filename
+    def prepare_file(self):  # 1) получение данных (тут может быть подготовка файла/его разахивация)
+        if self.file_name.endswith('.zip'):
+            zfile = zipfile.ZipFile(self.file_name, 'r')
+            self.file_name = ''
+            for filename in zfile.namelist():
+                zfile.extract(filename)
+            self.file_name = filename
+        with open(self.file_name, 'r', encoding='cp1251') as file:  # TODO перенес сюда, чтобы избежать большой
+            # TODO вложенности в collect_data()
+            self.collect_data(file)
 
-    def collect_data(self):  # 1) получение данных (тут может быть подготовка файла - его разахивация, при необходимости),
-        if self.file_name.endswith('.zip'):  # todo Эти две строки (то есть весь if) и есть "подготовка файла"
-            self.unzip()
-        with open(self.file_name, 'r', encoding='cp1251') as file:
-            for line in file:
-                self.collect_char(line)
-
-    def collect_char(self, line):  # 2) сбор даннных - подсчёт частоты использования букв,
-        for char in line:
-            if char.isalpha():
-                self.stat[char] = self.stat.get(char, 0) + 1
+    def collect_data(self, file):  # 2) сбор даннных - подсчёт частоты использования букв
+        for line in file:
+            for char in line:
+                if char.isalpha():
+                    self.stat[char] = self.stat.get(char, 0) + 1
 
     def sort_chars(self):  # 3) сортировка статистических данных,
         self.sorted_stat = sorted(self.stat.items(), key=operator.itemgetter(1), reverse=True)
-        #  забыли указать self. перед именем атрибута stat
-        #  если так делаю, то подсвечивается .items() и выдает ошибку Unresolved attribute reference 'items' for class 'list'
-        # todo Вы правы, просмотрел, что stat это словарь, а нужен список. Создаём новый атрибут-список для
-        #  сортированных данных, сохраняем результат сортировки в него и его используем в печати в консоль
         return self.sorted_stat
 
     def print_stat(self):  # 4) вывод в консоль таблицы с данными.
@@ -69,14 +64,7 @@ class StatMaker:
         print('|  буква  | частота  |')
         print('+---------+----------+')
         for item in self.sorted_stat:
-            #  итерируйте по атрибуту self.stat
-            #  выдает ошибку
-            #  File "/Users/valeriichirkov/PycharmProjects/python_base/lesson_009/01_char_stat.py", line 70, in print_stat
-            #     print(f'|{item[0]:^9}|{item[1]:^10}|')
-            # IndexError: string index out of range
-            # todo поправил на список, если есть вопросы почему так, то спрашивайте
-            print(f'|{item[0]:^9}|{item[1]:^10}|')  # На вот это жалуется, думаю из-за того что в sort_chars() нужно
-            # делать self.stat = ... return self.stat, но выдает ошибку, написал в туду.
+            print(f'|{item[0]:^9}|{item[1]:^10}|')
         print('+---------+----------+')
         values = self.stat.values()
         total_chars = sum(values)
@@ -84,14 +72,8 @@ class StatMaker:
         print('+---------+----------+')
 
     def launch(self):
-        # а) подготовка файла (разархивация, при необходимости)
-        self.collect_data()  # б) сбор данных из файла  #  по шаблонному методу должно быть так (ниже)
-        # self.collect_char(line)  #  как вызвать этот метод, если он уже вызывается в .open()?
-        #  значить open назовите "сбор данных" и именно его тут вызывайте
-        #  переименовал, но тогда подготовки файла получается нет
-        # todo под подготовкой данных пока может быть только разархивация если передан архив, поэтому создаём метод
-        #  "подготовка_файла", в него помещаем разархивацию по условую, а тут, в шаблонном методе перед сбором данных,
-        #  вызываем "подготовку файла"
+        self.prepare_file()  # а) подготовка файла (разархивация, при необходимости)
+        self.collect_data(file=self.file)  # б) сбор данных из файла
         self.sort_chars()  # в) сортировка
         self.print_stat()  # г) вывод в консоль
 
