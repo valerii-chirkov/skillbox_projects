8c87fc69f7e4e57a5ee72ecfa8cf9f5405748eca
Alexander Olhovik <aao.t@yandex.ru>
2020-08-29 14:24:25 +0300

    урок 9, есть недочеты

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -35,6 +35,7 @@ class StatMaker:
     def __init__(self, file_name):
         self.file_name = file_name
         self.stat = {}
+        self.sorted_stat = []
 
     def unzip(self):  # 1) получение данных (тут может быть подготовка файла - его разахивация, при необходимости),
         zfile = zipfile.ZipFile(self.file_name, 'r')
@@ -44,7 +45,7 @@ class StatMaker:
         self.file_name = filename
 
     def collect_data(self):  # 1) получение данных (тут может быть подготовка файла - его разахивация, при необходимости),
-        if self.file_name.endswith('.zip'):
+        if self.file_name.endswith('.zip'):  # todo Эти две строки (то есть весь if) и есть "подготовка файла"
             self.unzip()
         with open(self.file_name, 'r', encoding='cp1251') as file:
             for line in file:
@@ -56,21 +57,24 @@ class StatMaker:
                 self.stat[char] = self.stat.get(char, 0) + 1
 
     def sort_chars(self):  # 3) сортировка статистических данных,
-        stat = sorted(self.stat.items(), key=operator.itemgetter(1), reverse=True)
-        # todo забыли указать self. перед именем атрибута stat
-        # TODO если так делаю, то подсвечивается .items() и выдает ошибку Unresolved attribute reference 'items' for class 'list'
-        return stat
+        self.sorted_stat = sorted(self.stat.items(), key=operator.itemgetter(1), reverse=True)
+        #  забыли указать self. перед именем атрибута stat
+        #  если так делаю, то подсвечивается .items() и выдает ошибку Unresolved attribute reference 'items' for class 'list'
+        # todo Вы правы, просмотрел, что stat это словарь, а нужен список. Создаём новый атрибут-список для
+        #  сортированных данных, сохраняем результат сортировки в него и его используем в печати в консоль
+        return self.sorted_stat
 
     def print_stat(self):  # 4) вывод в консоль таблицы с данными.
         print('+---------+----------+')
         print('|  буква  | частота  |')
         print('+---------+----------+')
-        for item in self.sort_chars():
-            # todo итерируйте по атрибуту self.stat
-            # TODO выдает ошибку
-            # TODO File "/Users/valeriichirkov/PycharmProjects/python_base/lesson_009/01_char_stat.py", line 70, in print_stat
+        for item in self.sorted_stat:
+            #  итерируйте по атрибуту self.stat
+            #  выдает ошибку
+            #  File "/Users/valeriichirkov/PycharmProjects/python_base/lesson_009/01_char_stat.py", line 70, in print_stat
             #     print(f'|{item[0]:^9}|{item[1]:^10}|')
             # IndexError: string index out of range
+            # todo поправил на список, если есть вопросы почему так, то спрашивайте
             print(f'|{item[0]:^9}|{item[1]:^10}|')  # На вот это жалуется, думаю из-за того что в sort_chars() нужно
             # делать self.stat = ... return self.stat, но выдает ошибку, написал в туду.
         print('+---------+----------+')
@@ -83,8 +87,11 @@ class StatMaker:
         # а) подготовка файла (разархивация, при необходимости)
         self.collect_data()  # б) сбор данных из файла  #  по шаблонному методу должно быть так (ниже)
         # self.collect_char(line)  #  как вызвать этот метод, если он уже вызывается в .open()?
-        # todo значить open назовите "сбор данных" и именно его тут вызывайте
-        # TODO переименовал, но тогда подготовки файла получается нет
+        #  значить open назовите "сбор данных" и именно его тут вызывайте
+        #  переименовал, но тогда подготовки файла получается нет
+        # todo под подготовкой данных пока может быть только разархивация если передан архив, поэтому создаём метод
+        #  "подготовка_файла", в него помещаем разархивацию по условую, а тут, в шаблонном методе перед сбором данных,
+        #  вызываем "подготовку файла"
         self.sort_chars()  # в) сортировка
         self.print_stat()  # г) вывод в консоль
 
