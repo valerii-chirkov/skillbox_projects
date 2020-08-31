0330edd04e0facd9b474c3dcc596501a75aec194
Valerii <valerii_chirkov@bk.ru>
2020-08-31 22:46:12 +0700

    done

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -56,77 +56,55 @@ class StatMaker:
                 if char.isalpha():
                     self.stat[char] = self.stat.get(char, 0) + 1
 
-    def sort_chars(self):
-        # todo Одна из важных частей изучаемого паттерна - новая функциолнальность добавляется не универсиализацией
-        #  одного класса, а переопределением методов в наследниках. В данном случае тут надо оставить одну из сортировок,
-        #  а все новые методы сортировки расположить в наследниках. То есть, допустим в базовом классе оставляет
-        #  сортировку по убыванию, а для сортировки по возрастанию делаем наследника и переопределяем метод сортировки.
-        #  С остальными методам сортировки надо поступить также - каждый поместить в своего наследника базового класса.
-        while True:
-            print('1 - по частоте по убыванию')
-            print('2  - по частоте по возрастанию')
-            print('3  - по алфавиту по возрастанию')
-            print('4  - по алфавиту по убыванию')
-            self.sort_input = input('Введите способ сортировки: ')
-            if self.sort_input == '1':
-                self.sort_chars_frequency_decrease()
-                break
-            elif self.sort_input == '2':
-                self.sort_chars_frequency_increase()
-                break
-            elif self.sort_input == '3':
-                self.sort_chars_alphabet_increase()
-                break
-            elif self.sort_input == '4':
-                self.sort_chars_alphabet_decrease()
-                break
-            else:
-                print('Повторите попытку')
+    # def sort_chars(self):
+    #     # todo Одна из важных частей изучаемого паттерна - новая функциолнальность добавляется не универсиализацией
+    #     #  одного класса, а переопределением методов в наследниках. В данном случае тут надо оставить одну из сортировок,
+    #     #  а все новые методы сортировки расположить в наследниках. То есть, допустим в базовом классе оставляет
+    #     #  сортировку по убыванию, а для сортировки по возрастанию делаем наследника и переопределяем метод сортировки.
+    #     #  С остальными методам сортировки надо поступить также - каждый поместить в своего наследника базового класса.
+    #     while True:
+    #         print('1 - по частоте по убыванию')
+    #         print('2  - по частоте по возрастанию')
+    #         print('3  - по алфавиту по возрастанию')
+    #         print('4  - по алфавиту по убыванию')
+    #         self.sort_input = input('Введите способ сортировки: ')
+    #         if self.sort_input == '1':
+    #             self.sort_chars_frequency_decrease()
+    #             break
+    #         elif self.sort_input == '2':
+    #             self.sort_chars_frequency_increase()
+    #             break
+    #         elif self.sort_input == '3':
+    #             self.sort_chars_alphabet_increase()
+    #             break
+    #         elif self.sort_input == '4':
+    #             self.sort_chars_alphabet_decrease()
+    #             break
+    #         else:
+    #             print('Повторите попытку')
 
-    def sort_chars_frequency_decrease(self):  # 3) сортировка статистических данных (убывание по частоте)
+    def sort(self):  # 3) сортировка статистических данных (убывание по частоте)
         self.sorted_stat = sorted(self.stat.items(), key=operator.itemgetter(1), reverse=True)
         return self.sorted_stat
 
-    def sort_chars_frequency_increase(self):  # 3) сортировка статистических данных (возрастание по частоте)
-        self.sorted_stat = sorted(self.stat.items(), key=operator.itemgetter(1), reverse=False)
-        return self.sorted_stat
+    # def sort_chars_frequency_increase(self):  # 3) сортировка статистических данных (возрастание по частоте)
+    #     self.sorted_stat = sorted(self.stat.items(), key=operator.itemgetter(1), reverse=False)
+    #     return self.sorted_stat
 
-    def sort_chars_alphabet_increase(self):  # 3) сортировка статистических данных (возрастание по алфавиту)
-        self.stat_keys = list(self.stat.keys())
-        self.stat_keys.sort()
-        # self.stat_keys = list(self.stat.keys()).sort()  # так не работает
-        for key in self.stat_keys:
-            self.sorted_stat.append([key, self.stat[key]])  #  Он не совсем правильно сортирует, например буква ё в
-            #  конце, может быть потому что она идет как спецсимвол у букв?
-            # todo Символы Ёё находятся в стороне от остальных символов алфавита: 
-            #  >> ord('ё')
-            # Out[2]: 1105
-            # >> ord('е')
-            # Out[3]: 1077
-            # >> ord('я')
-            # Out[4]: 1103
-            # >> ord('Я')
-            # Out[5]: 1071
-            # >> ord('Ё')
-            # Out[6]: 1025
-            # todo Для данной задачи можно на этом не заострять внимание
+    # def sort_chars_alphabet_increase(self):  # 3) сортировка статистических данных (возрастание по алфавиту)
+    #     self.stat_keys = list(self.stat.keys())
+    #     self.stat_keys.sort()
+    #     for key in self.stat_keys:
+    #         self.sorted_stat.append([key, self.stat[key]])
+    #     return self.sorted_stat
 
-        return self.sorted_stat
-
-    def sort_chars_alphabet_decrease(self):  # 3) сортировка статистических данных (убывание по алфавиту)
-        self.stat_keys = list(self.stat.keys())
-        self.stat_keys.sort()
-        self.stat_keys.reverse()  # Есть ли в python'е что-то на подобие наследования от методов? Чтобы код не повторять
-                                  # todo можно только вызвать метод предка, и что-то добавить к этому нового, вот так:
-                                  #  def some_method()
-                                  #      super().some_method()
-                                  #      do some work
-                                  #      return something
-
-        for key in self.stat_keys:
-            self.sorted_stat.append(
-                [key, self.stat[key]])
-        return self.sorted_stat
+    # def sort_chars_alphabet_decrease(self):  # 3) сортировка статистических данных (убывание по алфавиту)
+    #     self.stat_keys = list(self.stat.keys())
+    #     self.stat_keys.sort()
+    #     self.stat_keys.reverse()
+    #     for key in self.stat_keys:
+    #         self.sorted_stat.append([key, self.stat[key]])
+    #     return self.sorted_stat
 
     def print_stat(self):  # 4) вывод в консоль таблицы с данными.
         print('+---------+----------+')
@@ -143,14 +121,41 @@ class StatMaker:
     def launch(self):
         self.prepare_file()  # а) подготовка файла (разархивация, при необходимости)
         self.collect_data(file=self.file)  # б) сбор данных из файла
-        self.sort_chars_alphabet_decrease()  # в) сортировка
-        self.sort_chars()
+        self.sort()  # в) сортировка
+        # self.sort_chars()
         self.print_stat()  # г) вывод в консоль
+
+
+class SortCharsFrequencyIncrease(StatMaker):
+    def sort(self):
+        self.sorted_stat = sorted(self.stat.items(), key=operator.itemgetter(1), reverse=False)
+        return self.sorted_stat
+
+
+class SortCharsAlphabetIncrease(StatMaker):  # 3) сортировка статистических данных (возрастание по алфавиту)
+    def sort(self):
+        self.stat_keys = list(self.stat.keys())
+        self.stat_keys.sort()
+        for key in self.stat_keys:
+            self.sorted_stat.append([key, self.stat[key]])
+        return self.sorted_stat
+
+
+class SortCharsAlphabetDecrease(StatMaker):
+    def sort_chars_alphabet_decrease(self):  # 3) сортировка статистических данных (убывание по алфавиту)
+        self.stat_keys = list(self.stat.keys())
+        self.stat_keys.sort()
+        self.stat_keys.reverse()
+        for key in self.stat_keys:
+            self.sorted_stat.append([key, self.stat[key]])
+        return self.sorted_stat
 
 
 statmaker = StatMaker(file_name=FILE)
 statmaker.launch()
-
+sort1 = SortCharsFrequencyIncrease
+sort2 = SortCharsAlphabetIncrease
+sort3 = SortCharsAlphabetDecrease
 # зачет первой части
 # После зачета первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
