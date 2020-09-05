6073d7ea9310a4bf799906150ee926aa8051e2d3
Valerii <valerii_chirkov@bk.ru>
2020-09-05 21:29:36 +0700

    done

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -36,7 +36,7 @@ class NOKParser:
         self.date = ''
         self.cut = None
 
-    def setup_cut(self):  # todo Вот этот самый метод, который будем переопределять
+    def setup_cut(self):
         self.cut = 17
 
     def fill_dict(self):
@@ -71,24 +71,27 @@ class GroupingHours(NOKParser):
         self.cut = 14
 
 
-# class GroupingDays(NOKParser):
-#     pass
-#
-#
-# class GroupingMonths(NOKParser):
-#     pass
-#
-#
-# class GroupingYears(NOKParser):
-#     pass
+class GroupingDays(NOKParser):
+    def setup_cut(self):
+        self.cut = 11
+
+
+class GroupingMonths(NOKParser):
+    def setup_cut(self):
+        self.cut = 8
+
+
+class GroupingYears(NOKParser):
+    def setup_cut(self):
+        self.cut = 5
 
 
 nokparser = NOKParser(file_name=FILE, out_file=OUT_FILE)
-# sort1 = GroupingHours(file_name=FILE, out_file=OUT_FILE, cut=14)
-# sort2 = GroupingDays(file_name=FILE, out_file=OUT_FILE, cut=11)
-# sort3 = GroupingMonths(file_name=FILE, out_file=OUT_FILE, cut=8)
-# sort4 = GroupingYears(file_name=FILE, out_file=OUT_FILE, cut=5)
-nokparser.launch()
+sort1 = GroupingHours(file_name=FILE, out_file=OUT_FILE)
+sort2 = GroupingDays(file_name=FILE, out_file=OUT_FILE)
+sort3 = GroupingMonths(file_name=FILE, out_file=OUT_FILE)
+sort4 = GroupingYears(file_name=FILE, out_file=OUT_FILE)
+sort3.launch()
 
 # После зачета первого этапа нужно сделать группировку событий
 #  - по часам
