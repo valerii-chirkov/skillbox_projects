1d5cf85c893563e5a8b2dc79839148c6ba4ba268
Valerii <valerii_chirkov@bk.ru>
2020-09-08 14:45:32 +0700

    done

--- lesson_010/01_fifth_element.py
+++ lesson_010/01_fifth_element.py
@@ -2,12 +2,21 @@
 
 # Умножить константу BRUCE_WILLIS на пятый элемент строки, введенный пользователем
 
-BRUCE_WILLIS = 42
+while True:
+    try:
+        BRUCE_WILLIS = 42
+        input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+        leeloo = int(input_data[4])
+        result = BRUCE_WILLIS * leeloo
+        print(f'- Leeloo Dallas! Multi-pass № {result}!')
+        break
+    except IndexError:
+        print('Введите более 5ти элементов')
+    except ValueError:
+        print('Введите только число')
+    except Exception:
+        print('Введите только число больше 5ти символов')
 
-input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f'- Leeloo Dallas! Multi-pass № {result}!')
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
