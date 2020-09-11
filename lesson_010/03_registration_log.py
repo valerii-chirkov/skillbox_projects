ff4ad691f3e5334918ee74fde4f1ff2c2e609769
Valerii <valerii_chirkov@bk.ru>
2020-09-11 15:17:49 +0700

    done

--- lesson_010/03_registration_log.py
+++ lesson_010/03_registration_log.py
@@ -22,4 +22,94 @@
 # - поле возраст НЕ является числом от 10 до 99: ValueError
 # Вызов метода обернуть в try-except.
 
-# TODO здесь ваш код
+FILE = 'registrations.txt'
+FILE_OUT_GOOD = 'registrations_good.log'
+FILE_OUT_BAD = 'registrations_bad.log'
+
+
+class NotNameError(Exception):
+    pass
+
+
+class NotEmailError(Exception):
+    pass
+
+
+def check_name(name):
+    if name.isalpha():
+        return True
+    else:
+        raise NotNameError('There is not only letters')
+
+
+def check_email(email):
+    if '.' and '@' in email:
+        return True
+    else:
+        raise NotNameError('There is not enough symbols')
+
+
+def check_age(age):
+    if age in range(10, 99):
+        return True
+    else:
+        raise ValueError('Value Error')
+
+
+with open(FILE, 'r') as ff:
+    for line in ff:
+        try:
+            name, email, age = line.split(' ')
+            name = str(name)
+            email = str(email)
+            age = int(age)
+            try:
+                if check_name(name) and check_email(email) and check_age(age):
+                    with open(FILE_OUT_GOOD, 'w') as file:
+                        file.write(line)
+
+            except Exception as ex:
+                with open(FILE_OUT_BAD, 'w') as file:
+                    file.write(line + str(ex))
+                    continue
+
+        except ValueError as ex:
+            with open(FILE_OUT_BAD, 'w') as file:
+                file.write(line + str(ex))
+                continue
+
+# def calc(line):
+#     # print(f'Read line {line}', flush=True)
+#     operand_1, operation, operand_2 = line.split(' ')
+#     operand_1 = int(operand_1)
+#     operand_2 = int(operand_2)
+#     if operation == '+':
+#         value = operand_1 + operand_2
+#     elif operation == '-':
+#         value = operand_1 - operand_2
+#     elif operation == '/':
+#         value = operand_1 / operand_2
+#     elif operation == '*':
+#         value = operand_1 * operand_2
+#     elif operation == '//':
+#         value = operand_1 // operand_2
+#     elif operation == '%':
+#         value = operand_1 % operand_2
+#     else:
+#         raise ValueError('Unknown operation {operation}')
+#     return value
+#
+#
+# total = 0
+# with open('calc.txt', 'r') as ff:
+#     for line in ff:
+#         line = line[:-1]
+#         try:
+#             total += calc(line)
+#         except ValueError as exc:
+#             if 'unpack' in exc.args[0]:
+#                 print(f'Не хватает операндов {exc} в строке {line}')
+#             else:
+#                 print(f'Не могу преобразовать к целому {exc} в строке {line}')
+#
+# print(f'Total {total}')
