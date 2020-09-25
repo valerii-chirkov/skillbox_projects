4b2a446c6968b9bff106e09c92b01def77caaacc
Valerii <valerii_chirkov@bk.ru>
2020-09-25 17:37:49 +0700

    done

--- lesson_011/01_shapes.py
+++ lesson_011/01_shapes.py
@@ -16,16 +16,12 @@ import simple_draw as sd
 def get_polygon(n):
     def draw_shape(point, angle, length):
         step = round(360 / n)
-        for delta in range(0, 360 + step, step):  #  не знаю, подойдет такой вариант или нет - OK, только уберите лишнюю
-            # сторону (+ step). А исправить несовпадение первой и последней точек надо другим способом - в цикле
-            # рисовать на одну cторону меньше, а после цикла рисовать линию (sd.line) между первой и последней точками
-            # vec = sd.vector(start=point, angle=delta + angle, length=length)  #  долго пытался вызвать им, но
-            # он рисовал 'звездочку', тк end_point почему-то не возвращал
-            # TODO sd.vector возвращает именно крайнюю точку, только её надо брать сразу, а не пытаться искать атрибуты:
-            #  point = sd.vector...
-            v = sd.Vector(start_point=point, direction=delta+angle, length=length)  # todo смысла менять get_vector на Vector нет
-            v.draw()
-            point = v.end_point
+        start_point = point  # TODO можно обойтись без этого и вызвать дефолтное значение?
+        for delta in range(0, 360 - step, step):
+            vector = sd.vector(start=point, angle=delta+angle, length=length)
+            point = vector
+        sd.line(start_point, point)  # sd.line(sd.get_point(200, 200), point)  # TODO так ведь не расширяемо?
+
     return draw_shape
 
 
