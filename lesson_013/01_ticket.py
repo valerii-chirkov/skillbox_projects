b3f2ebcfc28dfd4bd47e4f24f7c38d9fab90bf2c
Valerii <valerii_chirkov@bk.ru>
2021-01-27 11:11:11 +0700

    done

--- lesson_013/01_ticket.py
+++ lesson_013/01_ticket.py
@@ -7,11 +7,37 @@
 # Шаблон взять в файле lesson_013/images/ticket_template.png
 # Пример заполнения lesson_013/images/ticket_sample.png
 # Подходящий шрифт искать на сайте ofont.ru
+from PIL import Image, ImageDraw, ImageFont, ImageColor
 
-def make_ticket(fio, from_, to, date):
-    # TODO здесь ваш код
-    pass
+template = 'images/ticket_template.png'
+font_path = 'font_dud.ttf'
 
+
+def make_ticket(fio, from_, to, date, ):
+    # default_size = (672, 401)
+    im = Image.open(template)
+    draw = ImageDraw.Draw(im)
+    font = ImageFont.truetype(font_path, size=16)
+    params = fio, from_, to, date
+    positions = ([45, im.size[1] - 280],
+                 [45, im.size[1] - 280 + 70*1],
+                 [45, im.size[1] - 280 + 70*2],
+                 [285, im.size[1] - 280 + 70*2],)
+
+    def write(position, param):
+        draw.text(position, param, font=font, fill=ImageColor.colormap['black'])
+
+    # write each line
+    for i in range(len(params)):
+        write(position=positions[i], param=params[i])
+
+    # save
+    out_path = 'img.jpg'
+    im = im.convert('RGB')
+    im.save(out_path)
+
+
+make_ticket('Valerii Chirkov', 'Barnaul, Russia', 'Moscow, Russia', '27 Jan 2021')
 # Усложненное задание (делать по желанию).
 # Написать консольный скрипт c помощью встроенного python-модуля argparse.
 # Скрипт должен принимать параметры:
@@ -21,3 +47,4 @@ def make_ticket(fio, from_, to, date):
 #   --date - обязательный, когда летим.
 #   --save_to - необязательный, путь для сохранения заполненнего билета.
 # и заполнять билет.
+
