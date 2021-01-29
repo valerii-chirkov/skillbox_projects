e5f6a71296cc7febfcd25ecbf4a80e1fd6493863
Valerii <valerii_chirkov@bk.ru>
2021-01-29 17:49:25 +0700

    done

--- lesson_013/01_ticket.py
+++ lesson_013/01_ticket.py
@@ -13,26 +13,32 @@ template = 'images/ticket_template.png'
 font_path = 'font_dud.ttf'
 
 
-def make_ticket(fio, from_, to, date, ):
+def make_ticket(fio, from_, to, date):
     # default_size = (672, 401)
     im = Image.open(template)
     draw = ImageDraw.Draw(im)
     font = ImageFont.truetype(font_path, size=16)
-    params = fio, from_, to, date
-    positions = list()
+    params = 'fio', 'from_', 'to', 'date'  # TODO меня смущает, что без этого ничего не работает, можно же как-то
+    # TODO обойтись без этого и просто брать название аргументов?
 
     # coordinates iteration
-    for i in range(len(params)-1):  #  так?
-        # TODO Значительно удобнее был бы список словарей с ключами "текст" и "координаты"
-        positions.append([45, im.size[1] - 280 + 70*i])
-    positions.append([285, im.size[1] - 280 + 70*2])
+    filling_data = [
+        {'fio': fio, 'cords': (45, im.size[1] - 280 + 70*0)},
+        {'from_': from_, 'cords': (45, im.size[1] - 280 + 70 * 1)},
+        {'to': to, 'cords': (45, im.size[1] - 280 + 70 * 2)},
+        {'date': date, 'cords': (285, im.size[1] - 280 + 70*2)},
+    ]
 
+    # define a function to write information
     def write(position, param):
         draw.text(position, param, font=font, fill=ImageColor.colormap['black'])
 
     # write each line
-    for i in range(len(params)):
-        write(position=positions[i], param=params[i])
+    for i in range(len(filling_data)):
+        line = filling_data[i]
+        key = params[i]
+        _param = line.get(key)
+        write(position=filling_data[i]['cords'], param=_param)
 
     # save
     out_path = 'img.jpg'
@@ -40,7 +46,7 @@ def make_ticket(fio, from_, to, date, ):
     im.save(out_path)
 
 
-make_ticket('Valerii Chirkov', 'Barnaul, Russia', 'Moscow, Russia', '27 Jan 2021')
+make_ticket(fio='Valerii Chirkov', from_='Barnaul, Russia', to='Moscow, Russia', date='27 Jan 2021')
 # Усложненное задание (делать по желанию).
 # Написать консольный скрипт c помощью встроенного python-модуля argparse.
 # Скрипт должен принимать параметры:
