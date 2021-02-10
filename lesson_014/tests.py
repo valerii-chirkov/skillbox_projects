e31cef18c0c364f0dba99b1e9df9bf64a73f3cfd
Valerii <valerii_chirkov@bk.ru>
2021-02-10 17:28:53 +0700

    6 tests for
    test_zero
    test_ten_plus
    test_slash_first
    test_inappropriate_symbols
    test_even_odd
    test_10_frames

--- lesson_014/tests.py
+++ lesson_014/tests.py
@@ -3,21 +3,31 @@ from lesson_014.bowling import get_score
 
 
 class ScoreTest(unittest.TestCase):
+
     def test_zero(self):
-        game_result = get_score('1234XХ-102')
-        self.assertEqual(game_result, Exception)
+        with self.assertRaises(Exception):
+            get_score('1234XХ-102')
 
     def test_ten_plus(self):
-        game_result = get_score('1234XХ-199')
-        self.assertEqual(game_result, Exception)
+        with self.assertRaises(Exception):
+            get_score('1234XХ-199')
 
     def test_slash_first(self):
-        game_result = get_score('1234XХ-1/5')
-        self.assertEqual(game_result, Exception)
+        with self.assertRaises(TypeError):
+            get_score('1234XХ-1/5')
 
     def test_inappropriate_symbols(self):
-        game_result = get_score('1234XХ-1T7')
-        self.assertEqual(game_result, Exception)
+        with self.assertRaises(TypeError):
+            get_score('1234XХ-1T7')
+
+    def test_even_odd(self):
+        with self.assertRaises(ValueError):
+            get_score('12X12X12X1')
+
+    def test_10_frames(self):
+        with self.assertRaises(TypeError):
+            get_score('XXXXXXXXXXX')
+            get_score('1122334411223344-1--11')
 
 
 if __name__ == '__main__':
