0397e98d0e530f111264ef0d854a879caf9a3abe
Valerii <valerii_chirkov@bk.ru>
2021-02-12 00:36:05 +0700

    custom exc

---
+++ lesson_014/custom_exceptions.py
@@ -0,0 +1,18 @@
+class ValidSymbolsError(TypeError):
+    pass
+
+
+class TenFramesError(TypeError):
+    pass
+
+
+class SlashFirstError(TypeError):
+    pass
+
+
+class TenPointsFrameError(ValueError):
+    pass
+
+
+class OddEvenEqualityError(ValueError):
+    pass
