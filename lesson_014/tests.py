99f69223177e1750d2d7897409f9b73fa6cf0997
Valerii <valerii_chirkov@bk.ru>
2021-02-22 09:44:51 +0700

    Done

--- lesson_014/tests.py
+++ lesson_014/tests.py
@@ -1,6 +1,6 @@
 import unittest
-from lesson_014.bowling_refactored import Bowling as get_score_american
-from lesson_014.bowling import get_score as get_score_russian
+from lesson_014.bowling_refactored import get_score
+# from lesson_014.bowling import get_score
 from lesson_014.custom_exceptions import TenPointsFrameError, SlashFirstError, ValidSymbolsError, OddEvenEqualityError
 from lesson_014.custom_exceptions import TenFramesError
 
@@ -9,32 +9,32 @@ class ScoreTest(unittest.TestCase):
 
     def test_ten_plus(self):
         with self.assertRaises(TenPointsFrameError):
-            get_score_american('991234XХ-1')
+            get_score('99641234XХ-1')
 
     def test_slash_first(self):
         with self.assertRaises(SlashFirstError):
-            get_score_american('/51234XХ-1')
+            get_score('/51234XХ-1')
 
     def test_inappropriate_symbols(self):
         with self.assertRaises(ValidSymbolsError):
-            get_score_american('T1234')
+            get_score('T12344')
 
     def test_even_odd(self):
         with self.assertRaises(OddEvenEqualityError):
-            get_score_american('12X12X12X1')
+            get_score('12X12X12X1')
 
     def test_10_frames(self):
         with self.assertRaises(TenFramesError):
-            get_score_american('XXXXXXXXXXX')
-            get_score_american('1122334411223344-1--11')
+            get_score('XXXXXXXXXXX')
+            get_score('1122334411223344-1--11')
 
     def test_equal_score(self):
-        result_even_1 = get_score_american('Xx1234xX')  # 10+10+1 + 10+1+2 + 3 + 7 + 10+10 + 10 = 74
-        result_even_2 = get_score_american('XX9/4-4--3')  # 10+10+9 + 10+10 + 10+4 + 4 + 4 +3 = 74
+        result_even_1 = get_score('Xx1234xX')  # 10+10+1 + 10+1+2 + 3 + 7 + 10+10 + 10 = 74
+        result_even_2 = get_score('XX9/4-4--3')  # 10+10+9 + 10+10 + 10+4 + 4 + 4 +3 = 74
         self.assertEqual(result_even_1, result_even_2)
 
-        result_even_1 = get_score_american('Xx12125/5-1-')  # 61
-        result_even_2 = get_score_american('-9-9-9-9-9-9-7')  # 61
+        result_even_1 = get_score('Xx12125/5-1-')  # 61
+        result_even_2 = get_score('-9-9-9-9-9-9-7')  # 61
         self.assertEqual(result_even_1, result_even_2)
 
         # result_odd_1 = get_score('Xx1234x')
